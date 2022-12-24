f1e54ec7d248f3e6311c2e0f3ab6ba5a8515fba8
samcms1234 <samdevpro99@gmail.com>
2022-12-24 23:41:39 +0530

    Homepage with details added

--- src/components/Home.js
+++ src/components/Home.js
@@ -7,6 +7,52 @@ const Home = ({ home, provider, escrow, togglePop }) => {
 
     return (
         <div className="home">
+            <div className='home__details'>
+                <div className='home__image'>
+                    <img src={home.image} alt='Home' />
+                </div>
+
+                <div className='home__overview'>
+                    <h1>{home.name}</h1>
+                    <p>
+                        <strong>{home.attributes[2].value}</strong> bds |
+                        <strong>{home.attributes[3].value}</strong> ba |
+                        <strong>{home.attributes[4].value}</strong> sqft
+                    </p>
+                    <p>{home.address}</p>
+                    <h1>{home.attributes[0].value} ETH</h1>
+
+                    <div>
+                        <button className='home__buy'>
+                            Buy
+                        </button>
+                    </div>
+
+                    <button className='home__contact'>
+                        Contact Agent
+                    </button>
+
+                    <hr />
+
+                    <h2>Overview</h2>
+
+                    <h2>
+                        Facts and features
+                    </h2>
+
+                    <ul>
+                        {home.attributes.map((attribute, index) => (
+                            <li key={index}><strong>{attribute.trait_type}</strong> : {attribute.value}</li>
+                        ))}
+                    </ul>
+                </div>
+
+                <button onClick={togglePop} className='home__close' >
+                <img src={close} alt='close' />
+                </button>
+
+            </div>
+
         </div>
     );
 }
