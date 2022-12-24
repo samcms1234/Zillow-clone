6b5fb9ed54a1f6bd1e808c773736b9928213ac3d
samcms1234 <samdevpro99@gmail.com>
2022-12-24 23:40:52 +0530

    added home page for each cards

--- src/App.js
+++ src/App.js
@@ -23,6 +23,10 @@ function App() {
 
   const [ homes, setHomes ] = useState([]);
 
+  const [ home, setHome ] = useState({});
+
+  const [ toggle, setToggle ] = useState(false);
+
   const loadBlockchainData = async () => {
     const provider = new ethers.providers.Web3Provider(window.ethereum)
     setProvider(provider);
@@ -58,6 +62,11 @@ function App() {
     loadBlockchainData()
   }, [])
 
+  const togglePop = ( home ) => {
+    setHome(home);
+    toggle ? setToggle(false) : setToggle(true);
+  }
+
   return (
     <div>
 
@@ -72,7 +81,7 @@ function App() {
 
         <div className='cards'>
           {homes.map((home, index) => (
-            <div className='card' key={index}>
+            <div className='card' key={index} onClick={() => togglePop(home)}>
             <div className='card__image'>
               <img src={home.image} alt='Home' />
             </div>
@@ -90,6 +99,10 @@ function App() {
         </div>
 
       </div>
+
+      {toggle && (
+        <Home home={home} provider={provider} account={account} escrow={escrow} togglePop={togglePop} />
+      )}
 
     </div>
   );
